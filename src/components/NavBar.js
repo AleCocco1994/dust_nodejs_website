d5eabc0061c813776407e92b836c463b9da04ff7
Alessandro Cocco <alessandro94.cocco@gmail.com>
2023-10-22 00:02:13 -0400

    updated first page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,10 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React from 'react';
-import TwitterIcon from './Icons';
-import { motion } from "framer-motion" 
+import {TwitterIcon, IconPolimi, IconYoutube} from './Icons';
+import { motion } from 'framer-motion';
+
+const MotionLink = motion(Link)
 
 const CustomLink = ({href, title, className=""}) => {
     const router = useRouter();
@@ -10,13 +12,11 @@ const CustomLink = ({href, title, className=""}) => {
     return(
         <Link href={href} className={`${className} relative group`}>
             {title}
-
             <span className={`
                     h-[1px] inline-block w-full bg-dark 
                     absolute left-0 -bottom-0.5 
-                    group-hover:w-full 
-                    transition-[width] ease duration-300 
-                    ${router.asPath === href ? 'w-full' : 'w-0'} 
+                    group-hover:w-full transition-[width] ease duration-3000
+                    ${router.asPath === href ? 'w-full' : 'w-[0px]'} 
                     `}>
                 &nbsp;
             </span>
@@ -29,35 +29,29 @@ const NavBar = () => {
         <header
         className="w-full px-32 py-8 font-medium flex justify-between items-center"
         >
-            <nav>
-                <CustomLink href="/"         title="Home" className='mr-4'/>
-                <CustomLink href="/about"    title="About" className='mx-4'/>
-                <CustomLink href="/contacts" title="Contacts" className='mx-4'/>
-                <CustomLink href="/projects" title="Projects" className='mx-4'/>
-                <CustomLink href="/articles" title="Articles" className='ml-4'/>
+            <nav className="flex items-center justify-center flex-wrap"> 
+                <CustomLink href="/"            title="Home"       className='mr-4'/>
+                <CustomLink href="/about"       title="About"      className='mx-4'/>
+                <CustomLink href="/blog"        title="Blog"       className='mx-4'/>
+                <CustomLink href="/tutorial"    title="Tutorial"   className='mx-4'/>
+                <CustomLink href="/references"  title="References" className='ml-4'/>
             </nav>
             <nav className="flex items-center justify-center flex-wrap">
-                <motion.a href="https://twitter.com" target={"_blank"}
+                <motion.a href="https://www.polimi.it/" target={"_blank"}
                 whileHover={{y:-2}}
                 whileTap={{scale:0.9}}
-                className="w-6 mr-3">
-                    <TwitterIcon/>
+                className="w-10 mr-3">
+                    <IconPolimi/>
                 </motion.a>
 
-                <motion.a href="https://twitter.com" target={"_blank"}
+                <motion.a href="https://www.youtube.com/@dust-project" target={"_blank"}
                 whileHover={{y:-2}}
                 whileTap={{scale:0.9}}
-                className="w-6 mx-3">
-                    <TwitterIcon/>
+                className="w-10 mx-3">
+                    <IconYoutube/>
                 </motion.a>
 
-                <motion.a href="https://twitter.com" target={"_blank"}
-                whileHover={{y:-2}}
-                whileTap={{scale:0.9}}
-                className="w-6 mx-3">
-                    <TwitterIcon/>
-                </motion.a>
-
+                
                 <motion.a href="https://twitter.com" target={"_blank"}
                 whileHover={{y:-2}}
                 whileTap={{scale:0.9}}
@@ -68,10 +62,10 @@ const NavBar = () => {
             </nav> 
             <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
                 <div className="flex items-center justify-center mt-2">
-                <Link
+                <MotionLink
                     href="/"
                     className="w-16 h-16 bg-dark text-light flex items-center 
-                            justify-center rounded-full text-5x1 font-bold"
+                            justify-center rounded-full text-2xl font-bold"
                     whileHover={{
                     backgroundColor: [
                     "#121212",
@@ -83,7 +77,7 @@ const NavBar = () => {
                     ],
                     transition: { duration: 1, repeat: Infinity},
                     }}
-                    >AC</Link>
+                    >AC</MotionLink>
                 </div> 
             </div>
         </header>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,10 @@
+import Layout from '@/components/Layout'
+import AnimatedText from '@/components/AnimatedText'
 import Head from 'next/head'
+import Image from 'next/image'
+import profilePic from '../../public/images/profile/xv15_v7-removebg.png' // change this to your profile pic
+import Link from 'next/link'
+import {IconGitlab, IconPdf, IconMail, IconForum} from '@/components/Icons'
 
 export default function Home() {
   return (
@@ -7,8 +13,50 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className>
-        Home 
+      <main className="flex items-center text-dark w-full min-h-screen">
+        <Layout className='pt-0'>
+          <div className='flex items-center justify-between w-full'>
+            <div className='w-1/2'>
+              <Image src={profilePic} alt="CodeBucks" className='w-full h-auto'/>
+            </div>
+            <div className='w-1/2 flex flex-col item-center self-center'>
+              <AnimatedText text='DUST' className='!text-10xl !text-left'/> 
+              <AnimatedText text='A novel, flexible solution to solve aerodynamics problems.' className='!my-0 !text-4xl !text-justify !font-semibold'/>
+              <AnimatedText text='Engineered through a dynamic partnership between A3 by Airbus and Politecnico di Milano,  it is your gateway to affordable, customizable, and precise solutions for even the most complex aerodynamic configurations.' className='!my-0 !text-2xl !text-justify !font-semibold'/>
+              <div className='flex items-center self-start mt-2'>
+                <Link href="https://public.gitlab.polimi.it/DAER/dust" target={'_blank'} 
+                className='flex items-center bg-dark text-light 
+                          p-2.5 px-6 rounded-lg text-lg font-semibold
+                          hover:bg-light hover:text-dark 
+                          border-2 boder-solid boder-transparent hover:border-dark'>Repository 
+                <IconGitlab className='ml-2 w-6 h-6'/> 
+                </Link>
+                <Link href="https://public.gitlab.polimi.it/DAER/dust/-/raw/develop/DUST_user_manual/DUST_user_manual.pdf?ref_type=heads" target={'_blank'} 
+                className='mx-4 flex items-center bg-dark text-light 
+                          p-2.5 px-6 rounded-lg text-lg font-semibold
+                          hover:bg-light hover:text-dark 
+                          border-2 boder-solid boder-transparent hover:border-dark' 
+                          download={true}>Documentation
+                <IconPdf className='ml-2 w-6 h-6'/> 
+                </Link>
+                <Link href="mailto:info@dust-project.org" target={'_blank'}
+                className='mx-4 flex items-center bg-dark text-light 
+                p-2.5 px-6 rounded-lg text-lg font-semibold
+                hover:bg-light hover:text-dark 
+                border-2 boder-solid boder-transparent hover:border-dark'>Contact
+                <IconMail className='ml-2 w-6 h-6'/> 
+                </Link>
+                <Link href="https://community.dust-project.org/" target={'_blank'}
+                className='mr-4 flex items-center bg-dark text-light 
+                p-2.5 px-6 rounded-lg text-lg font-semibold
+                hover:bg-light hover:text-dark 
+                border-2 boder-solid boder-transparent hover:border-dark'>Community
+                <IconForum className='ml-2 w-6 h-6'/>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </Layout>
       </main>
     </>
   )
