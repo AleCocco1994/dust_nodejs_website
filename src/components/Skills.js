4767d83270d00292d44f2a766d2d044da120c09d
Alessandro Cocco <alessandro94.cocco@gmail.com>
2023-10-25 00:20:16 -0400

    wip with videos

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,46 +1,84 @@
-import React from "react";
-import {motion} from "framer-motion";
-import { Player, ControlBar, PlayToggle  } from 'video-react';
+import { useState, useRef, React } from "react";
+import { motion } from "framer-motion";
+//import { Player, ControlBar, PlayToggle  } from 'video-react';
 
 
-const Skill =({name, x, y}) => {
-    return(
-        <motion.div className="flex items-center justify-center rounded-3xl font-semibold text-light p-8
-                py-3 px-6 shadow-dark cursor-pointer absolute"
-                whileHover={{scale:1.05}}  
-                initial={{x:0,y:0}}
-                whileInView={{x:x,y:y}}
-                viewport={{once:true}}
-                transition={{duration:1.5}}
-                >
-                    {
-                        <Player className='w-[640px]' autoPlay={false} networkState={false} src={name}>
-                            <ControlBar autoHide={false} disableDefaultControls={true}>
-                                <PlayToggle />
-                            </ControlBar>
-                        </Player>
-                    }
-                </motion.div>
-    )
+const Skill = ({ name, x, y }) => {
 
+    const [isPlaying, setIsPlaying] = useState(false);
+    const videoRef = useRef(null);
+
+    const handleVideoClick = () => {
+        if (isPlaying) {
+            videoRef.current.pause();
+        } else {
+            videoRef.current.play();
+        }
+        setIsPlaying(!isPlaying);
+    };
+
+    const handlePauseClick = () => {
+        videoRef.current.pause();
+        setIsPlaying(false);
+    };
+
+    return (
+        <motion.div
+            className="flex items-center justify-center rounded-3xl font-semibold bg-blue-800 text-light p-8 py-3 px-3 shadow-dark cursor-pointer absolute"
+            whileHover={{ scale: 1.05 }}
+            initial={{ x: 0, y: 0 }}
+            whileInView={{ x: x, y: y }}
+            viewport={{ once: true }}
+            transition={{ duration: 1.5 }}
+        >
+            <div className="relative">
+                <video
+                    className="w-[600px]"
+                    src={name}
+                    onClick={handleVideoClick}
+                    ref={videoRef}
+                />
+                {!isPlaying && (
+                    <button
+                        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+                        onClick={() => setIsPlaying(true)}
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-12 w-12 text-white"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                        >
+                            <path d="M5 3l10 7-10 7z" />
+                        </svg>
+                    </button>
+                )}
+                
+                {isPlaying && (
+                    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></div>
+                )}
+                
+            </div>
+        </motion.div>
+    );
 }
 
 const Skills = () => {
     return (
         <>
-        <h2 className="font-bold text-8xl mt-64 w-full text-center">Skills</h2>
+            <h2 className="font-bold text-8xl mt-64 w-full text-center">Skills</h2>
             <div className="w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight">
 
                 <motion.div className="flex items-center justify-center rounded-full font-semibold bg-dark text-light p-8 shadow-dark
                 cursor-pointer"
-                whileHover={{scale:1.05}}  
+                    whileHover={{ scale: 1.05 }}
                 >
                     DUST
                 </motion.div>
-                <Skill name="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" x="20vw" y="6vw"/>
-                <Skill name="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" x="20vw" y="-22vw"/>
-                <Skill name="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" x="-20vw" y="6vw"/>
-                <Skill name="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" x="-20vw" y="-22vw"/>
+                <Skill name="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" x="20vw" y="12vw" />
+                <Skill name="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" x="20vw" y="-12vw" />
+                <Skill name="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" x="-20vw" y="-12vw" />
+                <Skill name="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" x="-20vw" y="12vw" />
             </div>
         </>
     )
